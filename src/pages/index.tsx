c628b3436828f7c3e600d5fa982b78663be81a31
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-12 00:57:10 +0530

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image'
-import { Lora } from 'next/font/google'
+import { Lora, Style_Script} from 'next/font/google'
 
 const lora = Lora({subsets: ['latin']})
+const style = Style_Script({ weight: '400', subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={lora.className}>      
-      <div >
-        Hello
-      </div>      
+    <main className={`${lora.className} bg-gray-300 px-5 flex flex-col items-center h-screen`}> 
+      <div className = 'flex flex-col items-center'>  
+        <div className = {`${style.className} text-3xl md:text-5xl lg:text-7xl xl:text-9xl mt-4 md:mt-8 lg:mt-12 xl:mt-16`}>Parth Choudhary</div> 
+        <div className = 'text-sm md:text-md lg:text-lg text-gray-700 mt-4'>Archive for poetry written by me. Add more description here.</div>
+      </div>     
     </main>
   )
 }
