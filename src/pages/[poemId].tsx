c05e3c7bd691c9a2c67337e2fad8d7bfc9fba320
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-12 14:12:56 +0530

    page

--- src/pages/[poemId].tsx
+++ src/pages/[poemId].tsx
@@ -14,11 +14,13 @@ export default function PoemItem({ poem, img }: { poem: PoemType, img: string })
             backgroundImage: img
         }} className = {`absolute top-0 left-0 w-screen h-screen mix-blend-darken brightness-50 bg-cover`}></div>
         <NavBar color="text-white" />
-        <div className = 'bg-white shadow-md absolute top-1/2 left-1/2 flex flex-col -translate-x-1/2 -translate-y-1/2 w-11/12 z-20 my-5 md:w-2/5 lg:w-1/4'>
-            <Link className = 'px-4 pt-4 pb-2 text-2xl' href = {'/'}><IoMdArrowBack/></Link>
-            <div className = 'tracking-wider text-gray-700 font-semibold text-2xl self-center bg-red-300'>{poem.title}</div>
-            <div>{poem.language}</div>
-            <div>{poem.poem}</div>
+        <div className = 'bg-white py-12 md:py-16 lg:py-24 shadow-md absolute top-1/2 left-1/2 flex flex-col -translate-x-1/2 -translate-y-1/2 w-11/12 z-20 my-5 md:w-2/5 lg:w-1/4'>
+            <Link className = 'px-4 pt-4 pb-2 text-2xl w-fit' href = {'/'}><IoMdArrowBack/></Link>
+            <div className = 'px-5 md:px-8 lg:px-12 text-2xl self-center mb-5'>{poem.title}</div>
+            <div className = 'px-5 md:px-8 lg:px-12 text-gray-800 font-semibold'>Language</div>
+            <div className = 'px-5 md:px-8 lg:px-12 text-sm text-gray-500'>{poem.language}</div>
+            <div className = 'px-5 md:px-8 lg:px-12 text-gray-800 font-semibold mt-3'>Poem</div>
+            <div className = 'px-5 md:px-8 lg:px-12 text-sm text-gray-500 mb-7'>{poem.poem}</div>
         </div>
     </div>
 }
@@ -44,5 +46,6 @@ export const getStaticProps: GetStaticProps<{ poem: PoemType }, { poemId: string
             poem,
             img: `url("/images/${poem.id}.jpg")`
         },
+        revalidate: 1000
     }
 }
